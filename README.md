594a3a45971b0575c04997e6029cf05b0f2278fe
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-08-27 21:53:37 -0400

    added usage. changed MIT license view on bottom

---
+++ README.md
@@ -0,0 +1,58 @@
+# ReadME generator 
+  [![License](https://img.shields.io/badge/License-MIT-green)](https://opensource.org/licenses/MIT)
+
+
+  # Description
+  
+  A node run readme creator
+  
+  # Table of Contents
+  
+  * [Install](#install)
+  
+  * [Usage](#usage)
+  
+  * [Contributing](#contributing)
+  
+  * [Tests](#tests)
+  
+  * [Questions](#questions)
+
+  * [Contact](#contact)
+  
+  ## Install
+  
+  Copy and paste 
+  
+  
+      npm install inquirer
+  
+  
+  ## Usage
+
+  ## Contributing
+
+  In order to contribute to this repo please **Fork and pull**
+  
+  ## Tests
+
+  Copy and paste to run tests 
+
+  
+    no tests needed
+  
+  
+  ## Questions
+  Github: [@KevinLaCarrubba](https://github.com/KevinLaCarrubba?tab=repositories)
+
+  Email: Kevin.LaCarrubba@yahoo.com
+  
+  ## License
+   Copyright 2021 Kevin LaCarrubba
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -25,7 +25,7 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license, name) {
   if (license == 'Apache2.0') {
-  return ` Copyright [2021] [${name}]
+  return ` Copyright 2021 ${name}
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -40,13 +40,13 @@ function renderLicenseSection(license, name) {
     limitations under the License.`
   }
 if (license == 'MIT') {
-return  ` Copyright [2021] [${name}]
+return  ` Copyright 2021 ${name}
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
 }
 
 if (license == 'None'){
@@ -92,6 +92,8 @@ function generateMarkdown(data) {
   
   
   ## Usage
+
+  ${data.useRepo}
 
   ## Contributing
 
